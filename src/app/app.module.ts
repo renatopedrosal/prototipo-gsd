d7aa1f170078a68163b4153215c04134580a3395
Renato Pedrosa Leite <renatopedrosaleite.rp@gmail.com>
2022-03-31 17:22:02 -0300

    Colocando icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing-module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, IndexComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
+    MatMenuModule,
+    MatToolbarModule,
     MatInputModule,
     MatFormFieldModule,
     MatIconModule,
