fcf549ae460b9c05de11ffc9e89bdd9837853729
Renato Pedrosa Leite <renatopedrosaleite.rp@gmail.com>
2022-07-25 16:33:55 -0300

    Criando cadastro usuário
    Criando novo componente cadastro-usuário

---
+++ src/app/modules/cadastro-usuario/cadastro-usuario.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CadastroUsuarioRoutingModule } from './cadastro-usuario-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CadastroUsuarioRoutingModule
+  ]
+})
+export class CadastroUsuarioModule { }
